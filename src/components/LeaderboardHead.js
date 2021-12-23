bcddb6adf904c2a5f1e94df8ffe9dcc8f3ae4fa4
karelvanoordt <63868555+karelvanoordt@users.noreply.github.com>
2021-12-23 18:44:53 -0500

    Fix linters

--- src/components/LeaderboardHead.js
+++ src/components/LeaderboardHead.js
@@ -5,12 +5,15 @@ const LeaderboardHeader = ({ id, year }) => {
   const seasons = [2020, 2019, 2018, 2017, 2016, 2015];
 
   return (
-    <div className='lead-head-cont'>
-      <div className='lead-title'>
+    <div className="lead-head-cont">
+      <div className="lead-title">
         <h2>Football360</h2>
       </div>
-      <div className='year-cont'>
-        <h3 className='lead-years'>Choose year:{' '}</h3>
+      <div className="year-cont">
+        <h3 className="lead-years">
+          Choose year:
+          {' '}
+        </h3>
         <select onChange={(e) => year(id, e.target.value)}>
           {seasons.map((season) => (
             <option key={season} value={season}>
